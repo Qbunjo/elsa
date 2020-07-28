c268258a6f5a919c3ec186146582b671ad108843
Qbunjo <39863704+Qbunjo@users.noreply.github.com>
2020-07-28 12:21:34 +0200

    Update fridge.ino

--- fridge.ino
+++ fridge.ino
@@ -2,51 +2,91 @@
 #include <Wire.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <LiquidCrystal_I2C.h>
 #define ONE_WIRE_BUS 2
 
 //initialize thermometer
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
 //var definitions
-int hist=2; //hysteresis
-int setTemp=8; //default set temperature after startup
+int hist = 2; //hysteresis
+int setTemp = 10; //default set temperature after startup
 
 
 //pin definitions
-int coolPin=10;
-int tempPin=8;
-int plusButton=6;
-int minusButton=7;
+int coolPin = 4;
+int plusButton = 9;
+int minusButton = 10;
 
-void coolMe(){
-	digitalWrite(coolPin,0);
+
+
+void TurnOn() {
+  lcd.display();
+  lcd.backlight();
 }
-void stopCooling(){
-	digitalWrite(coolPin,1);
+void TurnOff() {
+  lcd.noDisplay();
+  lcd.noBacklight();
 }
-int measureTemp(){
-	sensors.requestTemperatures();
-	float tempC = sensors.getTempCByIndex(0);
-	return tempC;
+
+void coolMe() {
+  digitalWrite(coolPin, 0);
 }
+void stopCooling() {
+  digitalWrite(coolPin, 1);
+}
+int measureTemp() {
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempCByIndex(0);
+  return tempC;
+}
+
+boolean ifPressed() {
+  if (digitalRead(plusButton) != 0 or digitalRead(minusButton) != 0)
+    return true;
+  else
+    return false;
+}
+
+void hysteresisChange(){
+  }
+
 
 void setup()
 {
-	sensors.begin();
-//define in/out pins
+  sensors.begin();
+  //define in/out pins
+  pinMode(coolPin, OUTPUT);
+  pinMode(plusButton, INPUT);
+  pinMode(minusButton, INPUT);
+  lcd.init();
+
+  // start lcd
+TurnOn();
 
 }
 
 
 void loop()
 {
-//measure temperature
-//display on the lcd
-if ((measureTemp()+hist)>setTemp){
-coolMe();
-}
-if ((measureTemp()+hist)<setTemp){
-	stopCooling();
-}
-
+  //measure temperature (main fridge process)
+  if ((measureTemp() + hist) > setTemp) {
+    coolMe();
+  }
+  if ((measureTemp() + hist) < setTemp) {
+    stopCooling();
+  }
+  //display on the lcd
+   lcd.setCursor(0, 1);
+    lcd.print("Set Temp:");
+    lcd.setCursor(9,1);
+    lcd.print(setTemp);
+   lcd.setCursor(0,0);
+   lcd.print("Cur Temp:");
+   lcd.setCursor(9,0);
+   lcd.print(measureTemp());
+  
 }
